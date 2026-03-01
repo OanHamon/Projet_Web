c09436fa168e6da3fe0c421b044fd28797f6ec2d
BastienLalande <bastien.lalande@viacesi.fr>
2026-03-01 16:01:15 +0100

    Add map functionality: integrate Leaflet map in search.html, update CSS for consistent styling, and set default font family in style.css.

---
+++ src/assets/script/map.js
@@ -0,0 +1,24 @@
+var map = L.map('map').setView([44, 5], 5);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+let marker;
+map.on('click', function (e) {
+    const lat = e.latlng.lat.toFixed(6);
+    const lng = e.latlng.lng.toFixed(6);
+
+    // Supprime l’ancien pin
+    if (marker) {
+    map.removeLayer(marker);
+    }
+
+    // Ajoute le pin
+    marker = L.marker([lat, lng]).addTo(map);
+
+    // Met à jour l'input
+    document.getElementById('location').value = lat +' , '+ lng;
+    }
+);
