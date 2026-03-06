452e32c1bd8dece6653442d954e29d7e3dd86706
OanHamon <oan.hamon@gmail.com>
2026-03-06 16:03:52 +0100

    Add base Controller and Model classes for application structure

---
+++ src/controllers/general_controller.php
@@ -0,0 +1,49 @@
+<?php 
+namespace App\Controllers;
+
+/**
+ * The base controller class for all controllers in the application.
+ */
+abstract class Controller {
+    /**
+     * The model associated with the controller.
+     *
+     * @var Model null
+     */
+    protected $model = null;
+
+    /**
+     * The template engine used by the controller.
+     *
+     * @var  \Twig\Environment null
+     */
+    protected $templateEngine = null;
+}
+
+
+class GeneralController extends Controller
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ src/models/general_model.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * The Model class is an abstract class that serves as the base class for all models in the application.
+ */
+abstract class Model {
+    protected $connection = null;
+}
+
+class GeneralModel extends Model
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
