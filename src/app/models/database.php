1f2e1396e70b350df4d183510f4b0ce56c524f91
OanHamon <oan.hamon@gmail.com>
2026-03-09 13:59:11 +0100

    Refactor model and controller structure: add BaseModel and GeneralController, remove old files

---
+++ src/app/models/database.php
@@ -0,0 +1,31 @@
+<?php
+
+class Database {
+
+    private $host = "localhost";
+    private $db_name = "welcome_to_the_ocean_bdd";
+    private $username = "oanha";
+    private $password = "ohMY2244.";
+
+    public $conn;
+
+    public function connect() {
+
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch(PDOException $exception) {
+            echo "Erreur connexion : " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
