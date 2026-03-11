f5b80a3e5e46231db6624ef228f894714afc0157
Louis Le Hel <louis.lehel@viacesi.fr>
2026-03-11 09:49:38 +0100

    ajout addtask fonction

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -11,7 +11,17 @@ class Base extends Controller
     
     public function welcomepage()
     {
-        $this->view->render('welcome');
+        $tasks = $this->model->getAllTasks();   
+        $this->view->render('welcome', ['tasks' => $tasks]);
     }
-
+    public function addTask(): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $title = $_POST['title'] ?? '';
+            $description = $_POST['description'] ?? '';
+            $this->model->addTask($title, $description);
+            header('Location: /welcome');
+            exit();
+        }
+    }
 }
