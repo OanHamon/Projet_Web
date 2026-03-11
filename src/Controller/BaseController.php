0aec57e30ccbf4c52387453e60b1e9af88986020
Louis Le Hel <louis.lehel@viacesi.fr>
2026-03-11 09:46:23 +0100

    Add BaseController with constructor and welcome page method

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -3,5 +3,15 @@
 
 class Base extends Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->model = new TaskModel();
+    }
     
+    public function welcomepage()
+    {
+        $this->view->render('welcome');
+    }
+
 }
