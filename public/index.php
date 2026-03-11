c2a74365759e4ca521c09672370e872aa1c16bd2
Louis Le Hel <louis.lehel@viacesi.fr>
2026-03-11 10:51:25 +0100

    test welcome page ( a essayer ) impossible d'avoir le site en local

--- public/index.php
+++ public/index.php
@@ -5,14 +5,10 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use App\Controller\BaseController;
+$controller = new BaseController();
+$controller->welcomepage();
 
-use App\Router\Router;
 
-$routeur = new Router();
 
-$routeur->add('GET','/test',[Controller::class, 'printTest']);
 
-$url    = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); // "/offres"
-$method = $_SERVER['REQUEST_METHOD'];     
-
-$routeur->dispatch($method,$url);
